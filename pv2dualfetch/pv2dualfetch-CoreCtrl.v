eac90f7428bc41b8001fa0248bcf99d63cf5258f
Wang-oss-tech <william.wang0817@gmail.com>
2026-03-30 11:33:09 -0400

    non-random suite passes, random suite is still failing branches

--- pv2dualfetch/pv2dualfetch-CoreCtrl.v
+++ pv2dualfetch/pv2dualfetch-CoreCtrl.v
@@ -160,7 +160,8 @@ module parc_CoreCtrl
   // Exception: if the currently-issued instruction is a D-stage
   // control transfer (j/jal/jr/jalr), we must allow F to update PC
   // immediately, otherwise the redirect gets dropped.
-  assign stall_Fhl = stall_Dhl || ( steering_mux_sel && !brj_taken_Dhl );
+  assign stall_Fhl = stall_Dhl
+                  || ( steering_mux_sel && !brj_taken_Dhl && !brj_taken_X0hl );
 
   // Next bubble bit
 
